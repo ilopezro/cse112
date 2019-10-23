fdd09ebce33d886bfa8c8fd04835796549e465b8
Isai Lopez <ilopezro@ucsc.edu>
2019-10-22 21:09:36 -0700

    cahnged file path

--- program1/tli.hs
+++ program1/tli.hs
@@ -61,7 +61,7 @@ parseTest []  st = ([], st)
 -- needs completing for partial credit
 
 main = do
-     pfile <- openFile ReadMode
+     pfile <- openFile FilePath ReadMode
      contents <- hGetContents pfile
      putStr (run (map parseLine (map words (lines contents))) [] "")
      hClose pfile
