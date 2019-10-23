47f27de5de305bfb5b70e3f6a98fa4745736a772
Isai Lopez <ilopezro@ucsc.edu>
2019-10-22 21:38:34 -0700

    changed

--- program1/tli.hs
+++ program1/tli.hs
@@ -14,8 +14,7 @@ data Stmt =
 -- dummy predicate that is supposed to check if a string is a label which is a string ending with ":"
 isLabel :: String -> Bool
 isLabel _ = False
-isLabel (": ") = True
-isLabel (x:xs) = isLabel xs
+isLabel (x:": ") = True
 
 -- takes a list of tokens as strings and returns the parsed expression
 parseExpr :: [String] -> Expr
