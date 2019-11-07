c8e0e16d7e23ac67300efecbdf7efb3065521653
Isai Lopez <ilopezro@ucsc.edu>
2019-11-07 00:07:32 -0800

    read

--- program2/prog2.pl
+++ program2/prog2.pl
@@ -49,10 +49,9 @@ test :- print_trip( depart, nyc, 'New York City', time( 9, 3)),
         
 doSomething(nyc,lax) :- test.
 
-main :- degrees_to_radians(33,39,output), 
-        format("~6f", [output]), nl.
+main :- read(X)
 
-
+# testDegToRad :- 
 
 /**
 * Airport db
