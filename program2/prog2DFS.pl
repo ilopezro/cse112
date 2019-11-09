dad6e5f9d33c461bd3961a7fe0fddaf99df55803
Isai Lopez <ilopezro@ucsc.edu>
2019-11-08 20:55:11 -0800

    changing methods

--- program2/prog2DFS.pl
+++ program2/prog2DFS.pl
@@ -169,11 +169,11 @@ printItinerary([flight( CurrAirport, NextAirport, time( Hours, Minutes)) | RestO
     print_trip(depart, CurrAirport, Leaving, time(Hours, Minutes)),
     haversine(Deg, Min, Deg3, Min3, Deg2, Min2, Deg4, Min4, Z), 
     convertToTime(Z, _, Hours1, Minutes1),
-    /*TotalHours is Hours + Hours1, 
-    TotalMinutes is Minutes + Minutes1, 
+    TotalHours is Hours + Hours1, 
+    /*TotalMinutes is Minutes + Minutes1, 
     write(TotalMinutes), nl,
     checkTime(TotalHours, TotalMinutes, FinalHours, FinalMinutes),*/
-    print_trip(arrive, NextAirport, Arriving, time(Hours+Hours1,Minutes+Minutes1)),
+    print_trip(arrive, NextAirport, Arriving, time(TotalHours,Minutes+Minutes1)),
     printItinerary(RestOfTrip).
 
 
