bebab0f3b3acdd1b66a56d7a335a7f4524475510
Isai Lopez <ilopezro@ucsc.edu>
2019-11-21 17:07:06 -0800

    got addition and equals working for edge cases

--- program3/tli.py
+++ program3/tli.py
@@ -23,28 +23,78 @@ class Expr :
         elif self.operator == "const":
             return symTable[op1]
         elif self.operator == "=":
-            tupleToAdd = (self.op1, self.op2)
-            symTable.append(tupleToAdd)
+            currTuple = list(filter(lambda x:self.op1 in x, symTable)) 
+            checkForVar2 = list(filter(lambda x:self.op2 in x, symTable)) 
+            if(len(currTuple) == 0):
+                try:
+                    int(self.op2)
+                    tupleToAdd = (self.op1, self.op2)
+                    symTable.append(tupleToAdd)
+                except:
+                    try:
+                        numToAdd = checkForVar2[0][1]
+                        tupleToAdd = (self.op1, float(numToAdd))
+                        symTable.append(tupleToAdd)
+                    except:
+                        sys.exit("Invalid Variable")
+            else:
+                try:
+                    int(self.op2)
+                    indexOfTuple = symTable.index(currTuple[0])
+                    newTuple = (self.op1, self.op2)
+                    symTable[indexOfTuple] = newTuple
+                except:
+                    try:
+                        numToAdd = checkForVar2[0][1]
+                        indexOfTuple = symTable.index(currTuple[0])
+                        newTuple = (self.op1, numToAdd)
+                        symTable[indexOfTuple] = newTuple
+                    except:
+                        sys.exit("Invalid Variable")
         elif self.operator == "+":
-            Output = list(filter(lambda x:self.op1 in x, symTable)) 
-            indexOfOutput = symTable.index(Output[0])
-            numToAdd = Output[0][1]
-            newTuple = (self.op1, float(self.op2) + float(numToAdd))
-            symTable[indexOfOutput] = newTuple
+            currTuple = list(filter(lambda x:self.op1 in x, symTable)) 
+            if(len(currTuple) == 0):
+                sys.exit("Invalid variable")
+            
+            checkForVar2 = list(filter(lambda x:self.op2 in x, symTable)) 
+
+            if len(checkForVar2) == 0:
+                try:
+                    int(self.op2)
+                    numToAdd = currTuple[0][1]
+                    result = float(self.op2) + float(numToAdd)
+                    return result
+                except:
+                    sys.exit("Invalid variable")
+            if len(checkForVar2) == 1:
+                try:
+                    int(self.op2)
+                    numToAdd = currTuple[0][1]
+                    result = float(self.op2) + float(numToAdd)
+                    return result
+                except:
+                    numToAdd = currTuple[0][1]
+                    num2 = checkForVar2[0][1]
+                    result = float(num2) + float(numToAdd)
+                    return result
         elif self.operator == "-":
-            Output = list(filter(lambda x:self.op1 in x, symTable)) 
-            indexOfOutput = symTable.index(Output[0])
-            numToAdd = Output[0][1]
-            newTuple = (self.op1, float(numToAdd) - float(self.op2))
-            symTable[indexOfOutput] = newTuple
+            currTuple = list(filter(lambda x:self.op1 in x, symTable)) 
+            indexOfTuple = symTable.index(currTuple[0])
+            numToAdd = currTuple[0][1]
+            result = float(numToAdd) - float(self.op1)
+            return result
         elif self.operator == "*":
             Output = list(filter(lambda x:self.op1 in x, symTable)) 
-            indexOfOutput = symTable.index(Output[0])
-            numToAdd = Output[0][1]
-            newTuple = (self.op1, float(self.op2) * float(numToAdd))
-            symTable[indexOfOutput] = newTuple
+            # indexOfOutput = symTable.index(Output[0])
+            # numToMult = Output[0][1]
+            # newTuple = (self.op1, float(self.op2) * float(numToMult))
+            # symTable[indexOfOutput] = newTuple
         elif self.operator == "/":
-            return symTable[op1]/symTable[op2]
+            Output = list(filter(lambda x:self.op1 in x, symTable)) 
+            # indexOfOutput = symTable.index(Output[0])
+            # numToDivide = Output[0][1]
+            # newTuple = (self.op1, float(self.op2)/float(numToDivide))
+            # symTable[indexOfOutput] = newTuple
         elif self.operator == "<":
             return symTable[op1] < symTable[op2]
         elif self.operator == ">":
@@ -92,7 +142,10 @@ class Stmt :
                 print("invalid expression")
             elif len(self.exprs) > 3:
                 expressionToEval = Expr(self.exprs[2], self.exprs[3], self.exprs[4])
-                expressionToEval.eval(symTable)
+                result = expressionToEval.eval(symTable)
+                del self.exprs[2:5]
+                self.exprs.append(result)
+                self.perform(symTable)
             else:
                 expressionToEval = Expr(self.exprs[0], self.exprs[1], self.exprs[2])
                 expressionToEval.eval(symTable)
