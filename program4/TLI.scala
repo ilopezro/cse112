b5a4dc093b204816de7dc6cc020fd45a40bbf5a5
Isai Lopez <ilopezro@ucsc.edu>
2019-12-06 18:43:45 -0800

    parse expr let works

--- program4/TLI.scala
+++ program4/TLI.scala
@@ -19,8 +19,14 @@ case class Print(exprList: ListBuffer[Expr]) extends Stmt
 object TLI {
     def eval(expr: Expr, symTab: Map[String, Double], lineNum: Double): Double = expr match {
         case BinOp("+",e1,e2) => eval(e1,symTab, lineNum) + eval(e2,symTab, lineNum) 
-        case Var(expr) => println("expr"); return 1
-        case Constant(expr) => println("num"); return 1
+        case Var(expr) => {
+            //return expression if it exists, if not error
+            println(expr);
+            return 1
+        }
+        case Constant(expr) => {
+            return expr; 
+        }
 	    case _ => System.exit(0); return -1 // should really throw an error
     }
 
@@ -40,9 +46,16 @@ object TLI {
             return lineNum;
         }
         case Print(list) => {
-            for(i <- list){
-                println(i)
-            }
+            //
+            println("hi from print")
+            return lineNum;
+        }
+        case Let(str, expr) =>{
+            println("hello from let")
+            return lineNum; 
+        }
+        case If(expr, label) => {
+            println("hello from if")
             return lineNum;
         }
         case _ => println("idk some error message here"); return -1; 
@@ -53,7 +66,54 @@ object TLI {
             if(line(0) == ""){
                 break
             }else if(line(0) == "let"){
-                // stmtList += (counter -> Let(line(1)))
+                //Let(var, expr)
+                var varToInsert = line(1); 
+                var restOfExpr = line.drop(2); 
+                if(restOfExpr.length == 2){
+                    if(restOfExpr(1).forall(_.isDigit)){
+                        var const = Constant(restOfExpr(1).toDouble)
+                        stmtList += (counter -> Let(varToInsert, const))
+                    }else{
+                        var variable = Var(restOfExpr(1));
+                        stmtList += (counter -> Let(varToInsert, variable))
+                    }
+                }else if(restOfExpr.length == 4){
+                    var leftConst = Constant(0); 
+                    var leftVar = Var("");
+                    var rightConst = Constant(0);
+                    var rightVar = Var("")
+                    var isLeftVar = false; 
+                    var isRigthVar = false; 
+                    if(restOfExpr(1).forall(_.isDigit)){
+                        leftConst = Constant(restOfExpr(1).toDouble)
+                    }else{
+                        leftVar = Var(restOfExpr(1))
+                        isLeftVar = true; 
+                    }
+                    if(restOfExpr(3).forall(_.isDigit)){
+                        rightConst = Constant(restOfExpr(3).toDouble)
+                    }else{
+                        rightVar = Var(restOfExpr(3))
+                        isRigthVar = true; 
+                    }
+
+                    if(isLeftVar && isRigthVar){
+                        var binaryOp = BinOp(restOfExpr(2), leftVar, rightVar)
+                        stmtList += (counter -> Let(varToInsert, binaryOp))
+                    }else if(isLeftVar){
+                        var binaryOp = BinOp(restOfExpr(2), leftVar, rightConst)
+                        stmtList += (counter -> Let(varToInsert, binaryOp))
+                    }else if(isRigthVar){
+                        var binaryOp = BinOp(restOfExpr(2), leftConst, rightVar)
+                        stmtList += (counter -> Let(varToInsert, binaryOp))
+                    }else{
+                        var binaryOp = BinOp(restOfExpr(2), leftConst, rightConst)
+                        stmtList += (counter -> Let(varToInsert, binaryOp))
+                    }
+                }else{
+                    println(s"Syntax error on line $counter.")
+                    System.exit(0)
+                }
             }else if(line(0) == "if"){
 
             }else if(line(0) == "print"){
@@ -74,7 +134,6 @@ object TLI {
                         exprList += Var(i)
                     }
                 }
-
                 stmtList += (counter -> Print(exprList))
             }else if(line(0) == "input"){
                 stmtList += (counter -> Input(line(1)));
@@ -112,13 +171,16 @@ object TLI {
         }
 
         var stmtListKeySet = statementList.keySet; 
-        var i = 0; 
 
-        statementList get 10 match {
-            case Some(Input(i)) => perform(Input(i), symTable, 8);
-            case Some(Print(list)) => perform(Print(list), symTable, 10);
-            case _ => println("still debugging")
+        for(i <- stmtListKeySet) { 
+            Some(statementList(i)) match {
+                case Some(Input(i)) => perform(Input(i), symTable, 8);
+                case Some(Print(list)) => perform(Print(list), symTable, 10);
+                case Some(Let(str, expr)) => println("we in let. leggo")
+                case _ => println("still debugging")
+            }
         }
+
         // println(stmt)
 
 
